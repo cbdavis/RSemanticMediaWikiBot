b4ca8eb209799de47ba7eac98890b7c09aba7269
Chris Davis <cbdavis@cbdavis-desktop.(none)>
2012-11-02 10:20:23 +0100

    setting up code to reuse edit token

--- edit.R
+++ edit.R
@@ -1,40 +1,52 @@
-edit <- function(title, text="", bot, summary="", minor=FALSE){
-
-  if(is.logical(minor)){
-    minor = as.character(minor)
-  }
-  
-  editResponse1 = postForm(bot$apiURL,
+getEditToken <- function(bot, title){
+  editResponse = postForm(bot$apiURL,
                            action="query",
                            format="xml",
                            prop="info",
                            titles=title,
                            intoken="edit",
                            .opts=bot$curlOpts)
-  
-  #looks like we can edit now
-  
-  doc = xmlParse(editResponse1, useInternalNodes=TRUE)
-  edittoken <- unlist(getNodeSet(doc, "//api/query/pages/page/@edittoken", addFinalizer=FALSE))
-  starttimestamp <- unlist(getNodeSet(doc, "//api/query/pages/page/@starttimestamp", addFinalizer=FALSE))
-  touched <- unlist(getNodeSet(doc, "//api/query/pages/page/@touched", addFinalizer=FALSE))
+
+  doc = xmlParse(editResponse, useInternalNodes=TRUE)
+  bot$edittoken <- unlist(getNodeSet(doc, "//api/query/pages/page/@edittoken", addFinalizer=FALSE))
+
+  #TODO read documentation (http://www.mediawiki.org/wiki/API:Edit_-_Create%26Edit_pages)
+  #starttimestamp and touched may only apply to a specific title
+  #while the edit token may be used for editing multiple pages.  Problems may arise from this.
+  bot$starttimestamp <- unlist(getNodeSet(doc, "//api/query/pages/page/@starttimestamp", addFinalizer=FALSE))
+  bot$touched <- unlist(getNodeSet(doc, "//api/query/pages/page/@touched", addFinalizer=FALSE))
   free(doc)
   rm(doc)
+  return(bot)
+}
+
+edit <- function(title, text="", bot, summary="", minor=FALSE){
+
+  if(is.logical(minor)){
+    minor = as.character(minor)
+  }
+  
+  #TODO no check is done to see if token is stale
+  #get an edit token if we don't have one already
+  if (is.null(bot$edittoken)){
+    bot = getEditToken(bot, title)  
+  }
+  
   
   #can pass:
   #summary
   #minor
   #title (or) pageid
   
-  editResponse2 = postForm(bot$apiURL,
+  editResponse = postForm(bot$apiURL,
                            action="edit",
                            format="xml",
                            prop="info",
                            title=title,
                            bot="true",
-                           token=edittoken,
-                           basetimestamp=touched,
-                           starttimestamp=starttimestamp,
+                           token=bot$edittoken,
+                           basetimestamp=bot$touched,
+                           starttimestamp=bot$starttimestamp,
                            summary=summary,
                            minor=minor,
                            text=text,
