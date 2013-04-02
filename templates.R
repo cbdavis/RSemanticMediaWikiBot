cfe79d444863b505a5441949c87fc14b24bf03e0
Chris Davis <cbdavis@cbdavis-desktop.(none)>
2013-04-02 07:43:54 +0200

    updating templates code

--- templates.R
+++ templates.R
@@ -7,63 +7,69 @@ extractTemplates <- function(pageTitle, bot){
   #read the contents of the page
   textVal = read(title=pageTitle, bot)
   
-  #extract all the templates
-  matchingInfo = gregexpr("\\{\\{[^\\}]+\\}\\}", textVal)
-  templateStartLocs = as.numeric(matchingInfo[[1]])
-  templateEndLocs = attr(matchingInfo[[1]], "match.length")
+  if (is.na(textVal)){
+    return(NA)
+  } else {
+    #extract all the templates
+    matchingInfo = gregexpr("\\{\\{[^\\}]+\\}\\}", textVal)
+    templateStartLocs = as.numeric(matchingInfo[[1]])
+    templateEndLocs = attr(matchingInfo[[1]], "match.length")
+    
+    allTemplateInfo = NULL
+    
+    #TODO what about uppercase/lowercase names for template parameters?
+    #TODO what is no parameters listed in the template?
+    #Check if only one parameter set also
+    #Check if newline characters are preserved
+    for (i in c(1:length(templateStartLocs))){
+      start = templateStartLocs[i]    
+      end = start + templateEndLocs[i] - 1
+      text = substring(textVal, start, end)
+      
+      #break the template into its parts
+      parametersAndValues = strsplit(text, "\\|")[[1]]
+      
+      #extract name of template
+      templateName = gsub("\n", "", gsub("\\{\\{", "", parametersAndValues[1]))
+      #replace spaces with underscores
+      #this helps to make names consistent if multiple templates of the same type are included, but use both variations for the name
+      templateName = gsub(" ", "_", templateName)
+      
+      #remove the part with the name of the template
+      parametersAndValues = tail(parametersAndValues, n=-1L)
+      
+      #remove the }} ending the template from the last parameter
+      parametersAndValues[length(parametersAndValues)] = gsub("\\}\\}", "", parametersAndValues[length(parametersAndValues)])
+      
+      parametersAndValues = gsub("\\n$", "", parametersAndValues)
   
-  allTemplateInfo = NULL
-  
-  #TODO what about uppercase/lowercase names for template parameters?
-  #TODO what is no parameters listed in the template?
-  #Check if only one parameter set also
-  #Check if newline characters are preserved
-  for (i in c(1:length(templateStartLocs))){
-    start = templateStartLocs[i]    
-    end = start + templateEndLocs[i] - 1
-    text = substring(textVal, start, end)
+      data = NULL
+      
+      if (length(parametersAndValues) > 0){
+        #convert this all into a data frame
+        parametersAndValues = colsplit(parametersAndValues, split="=", names=c('parameter', 'value'))
+      
+        #store this in a vector with names that we can reference
+        #also remove leading and trailing white space
+        data = gsub(" +$", "", gsub("^ +", "", as.character(parametersAndValues[,2])))
+        names(data) = gsub(" +$", "", gsub("^ +", "", parametersAndValues[,1]))
     
-    #break the template into its parts
-    parametersAndValues = strsplit(text, "\\|")[[1]]
-    
-    #extract name of template
-    templateName = gsub("\n", "", gsub("\\{\\{", "", parametersAndValues[1]))
-    #replace spaces with underscores
-    #this helps to make names consistent if multiple templates of the same type are included, but use both variations for the name
-    templateName = gsub(" ", "_", templateName)
-    
-    #remove the part with the name of the template
-    parametersAndValues = tail(parametersAndValues, n=-1L)
-    
-    #remove the }} ending the template from the last parameter
-    parametersAndValues[length(parametersAndValues)] = gsub("\\}\\}", "", parametersAndValues[length(parametersAndValues)])
-    
-    parametersAndValues = gsub("\\n$", "", parametersAndValues)
-
-    data = NULL
-    
-    if (length(parametersAndValues) > 0){
-      #convert this all into a data frame
-      parametersAndValues = colsplit(parametersAndValues, split="=", names=c('parameter', 'value'))
-    
-      #store this in a vector with names that we can reference
-      #also remove leading and trailing white space
-      data = gsub(" +$", "", gsub("^ +", "", as.character(parametersAndValues[,2])))
-      names(data) = gsub(" +$", "", gsub("^ +", "", parametersAndValues[,1]))
-  
-      #When there is an empty values for a template parameter, the value shows up as the name of the template parameter
-      locs = which(data == names(data)) #find entries with this issue
-      data[locs] = "" #set value to empty string
-      #Convert to a list.  This allows us to reference values via data$point, etc.
-      data = as.list(data)
+        #When there is an empty values for a template parameter, the value shows up as the name of the template parameter
+        locs = which(data == names(data)) #find entries with this issue
+        data[locs] = "" #set value to empty string
+        #Convert to a list.  This allows us to reference values via data$point, etc.
+        data = as.list(data)
+      }
+      templateInfo = list(textVal, pageTitle, templateName, start, end, text, parametersAndValues, data)
+      names(templateInfo) = c("pageText", "pageTitle", "name", "start", "end", "text", "parametersAndValues", "data")
+      
+      #allTemplateInfo = c(allTemplateInfo, list(templateInfo))
+      allTemplateInfo = c(allTemplateInfo, templateInfo)
     }
-    templateInfo = list(textVal, pageTitle, templateName, start, end, text, parametersAndValues, data)
-    names(templateInfo) = c("pageText", "pageTitle", "name", "start", "end", "text", "parametersAndValues", "data")
+      
+    return(allTemplateInfo)
     
-    allTemplateInfo = c(allTemplateInfo, list(templateInfo))
   }
-    
-  return(allTemplateInfo)
 }
 
 #This can return multiple templates if you are using multiple instance templates
@@ -73,8 +79,10 @@ getTemplateByName <- function(templateName, pageTitle, bot){
   templateName = gsub(" ", "_", templateName) #spaces are converted to underscores to ensure consistent matching
   templatesToReturn = NULL
   for (templateInfo in allTemplateInfo){
-    if(templateInfo$name == templateName){
-      templatesToReturn = c(templatesToReturn, list(templateInfo))
+    if(!is.na(templateInfo$name)){
+      if(templateInfo$name == templateName){
+        templatesToReturn = c(templatesToReturn, list(templateInfo))
+      }
     }
   }
   return(templatesToReturn)
