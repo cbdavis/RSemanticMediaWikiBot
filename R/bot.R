9131fad302eb26ed1c67787df2cbda447f38a94f
Chris Davis <c_b_dvs@yahoo.com>
2013-07-29 14:53:36 +0200

    option to add username and password for httpauth

--- R/bot.R
+++ R/bot.R
@@ -5,12 +5,17 @@ library(reshape2) #colsplit for working with template parameters and their value
 #never ever convert strings to factors
 options(stringsAsFactors = FALSE)
 
-initializeBot <- function(apiURL, verbose=FALSE){
+initializeBot <- function(apiURL, verbose=FALSE, userpwd = ""){
   curlOpts = curlOptions(verbose=verbose, #useful for debugging weirdness
                        header=TRUE, 
                        httpheader=c("Expect:"), # Needed if you have a proxy server such as squid set up, see http://stackoverflow.com/questions/6244578/curl-post-file-behind-a-proxy-returns-error
                        cookiefile="cookies.txt", #Cookies are required for managing sessions
                        cookiejar="cookies.txt")
+  
+  if (userpwd != ""){
+    curlOpts[["httpauth"]] = 1L
+    curlOpts[["userpwd"]] = userpwd
+  }
   
   bot = list(apiURL, curlOpts)
   names(bot) = c("apiURL", "curlOpts")
